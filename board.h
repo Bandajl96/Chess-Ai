56f277c90fec3409ebb8da70de803f690d0004b9
Jose Banda <joselbanda1996@live.com>
2020-04-25 01:33:43 -0500

    moved folder and downloaded msvc

---
+++ board.h
@@ -0,0 +1,50 @@
+//This file creates the chess board and defines the possible moves the pieces can make
+#include <iostream> 
+#include <vector> // for 2D vector 
+using namespace std; 
+
+
+//Defines the Board 
+
+//Defines the pieces for each player
+class Player {
+    public:
+
+};
+
+//Defines the Pawn class
+class Pawn {
+    public:
+
+};
+
+//Defines the Rook class
+class Rook {
+    public:
+
+};
+
+//Defines the Bishop class
+class Bishop {
+    public:
+
+};
+
+//Defines the Knight class
+class Knight {
+    public:
+
+};
+
+//Defines the Queen class
+class Queen {
+    public:
+
+};
+
+//Defines the King class
+class King {
+    public:
+
+};
+
